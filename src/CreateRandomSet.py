9b3e316ed94b0eb17b6ae8ddd98a6c20955462fa
Nicola Ferralis <feranick@hotmail.com>
2020-01-15 16:51:41 -0500

    First comit

---
+++ src/CreateRandomSet.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+**********************************************************
+* CreateRandomSet
+* 20200115a
+* By: Nicola Ferralis <feranick@hotmail.com>
+***********************************************************
+'''
+print(__doc__)
+
+import numpy as np
+import ast, sys
+from datetime import datetime, date
+
+def main():
+    if len(sys.argv) < 2:
+        print("usage: CreateRandomSets <number of students> <probabilities in array form - [.5, .3, .6, ...]>")
+        return
+    
+    prob_q = ast.literal_eval(sys.argv[2])
+    print("Attributes:",len(prob_q))
+    print("Probabilities:", prob_q,"\n")
+    print("Number of Students:",int(sys.argv[1]))
+
+    saved_roster = "roster"+str(datetime.now().strftime('_%Y-%m-%d_%H-%M-%S.csv'))
+
+    for i in range(int(sys.argv[1])):
+        kid = [i]
+        for j in range(len(prob_q)):
+            kid = np.hstack((kid, np.random.choice(np.arange(0,2),p=[prob_q[j],1-prob_q[j]])))
+        try:
+            kids = np.vstack((kids, kid))
+        except:
+            kids = kid
+    
+    print("Roster:\n", kids)
+    with open(saved_roster, "a") as file:
+        np.savetxt(file, kids, delimiter=',', fmt='%i')
+    print("\nRoster saved in:", saved_roster)
+    
+    
+#************************************
+''' Main initialization routine '''
+#************************************
+if __name__ == "__main__":
+    sys.exit(main())
+
+
+
---
+++ src/VerifyRandomSets.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+**********************************************************
+* VerifyRandomSets
+* 20200115a
+* By: Nicola Ferralis <feranick@hotmail.com>
+***********************************************************
+'''
+print(__doc__)
+
+import numpy as np
+import ast, sys, os.path
+
+def main():
+    if len(sys.argv) < 1:
+        print("usage: VerifyRandomSets roster_2020-01-15_15-49-14.csv")
+        return
+    else:
+        kids = np.genfromtxt(sys.argv[1], delimiter=',')
+    
+    print("Roster:\n", kids)
+
+    print("\n Input criteria for selection in the format:",repr(np.ones((kids.shape[1]-1), dtype=int).tolist()))
+    print(" where each element of the array can be 0 or 1, for attribute no and yes respectively,\n or 2, if it's irrelevant to the game")
+    print(" type 0 to exit ")
+    while True:
+        try:
+            sel_criteria = ast.literal_eval(input('\nCriteria: '))
+            if sel_criteria == 0:
+                break
+            selected = kids
+            for k in range(kids.shape[1]-1):
+                if sel_criteria[k] < 2:
+                    selected = selected[(selected[:,k+1] == sel_criteria[k])]
+
+            print("\nSelection criteria: ", sel_criteria)
+            print("Selected: ", selected)
+        except:
+            print("wrong format")
+            
+#************************************
+''' Main initialization routine '''
+#************************************
+if __name__ == "__main__":
+    sys.exit(main())
+
